160da50dc329ed86b9e6f4e925ccf12484f648fa
eslustig <35883235+eslustig@users.noreply.github.com>
2018-10-23 15:43:46 -0400

    Add files via upload

--- project1-206.py
+++ project1-206.py
@@ -2,13 +2,12 @@ import os
 import filecmp
 from dateutil.relativedelta import *
 from datetime import date
-
+import csv
 
 def getData(file):
-# get a list of dictionary objects from the file
-#Input: file name
-#Ouput: return a list of dictionary objects where
-#the keys are from the first row in the data. and the values are each of the other rows
+	inFile = open(file,"r")
+	lines = inFile.readlines()
+	inFile.close()
 
 	pass
 
