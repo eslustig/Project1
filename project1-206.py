c8093ae11ddedfbdd4b7123edfb621d7003e9f29
Eli_L <eslustig@umich.edu>
2018-10-23 17:18:24 -0400

    functions working

--- project1-206.py
+++ project1-206.py
@@ -4,55 +4,88 @@ from dateutil.relativedelta import *
 from datetime import date
 import csv
 
+
 def getData(file):
-	inFile = open(file,"r")
-	lines = inFile.readlines()
-	inFile.close()
 
-	pass
+	input_file = open(file,"r")
+	lines = input_file.readlines()
+	input_file.close()
 
-def mySort(data,col):
-# Sort based on key/column
-#Input: list of dictionaries and col (key) to sort on
-#Output: Return the first item in the sorted list as a string of just: firstName lastName
+	my_dicts = []
+	
+	for line in lines[1:]:
+	    student_dict = {}
 
-	pass
+	    items = line.split(',')
+	    first = items[0]
+	    last = items[1]
+	    email = items[2]
+	    Year = items[3]
+	    DOB = items[4]
 
+	    student_dict['First'] = first
+	    student_dict['Last'] = last
+	    student_dict['Email'] = email
+	    student_dict['DOB'] = DOB
+	    student_dict['Class'] = Year
+	    my_dicts.append(student_dict)
+	return my_dicts
+
+
+def mySort(data, col):
+	sorted_students = sorted(data, key =  lambda x: x[col])
+	that_student = sorted_students[0]
+	return that_student['First'] + " " + that_student['Last']
 
 def classSizes(data):
-# Create a histogram
-# Input: list of dictionaries
-# Output: Return a list of tuples sorted by the number of students in that class in
-# descending order
-# [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
-
-	pass
-
+	senior_size = 0
+	junior_size = 0
+	sophomore_size = 0
+	freshman_size = 0 
+	for student in data:
+		if student["Class"] == "Senior":
+			senior_size += 1
+		elif student["Class"] == "Junior":
+			junior_size += 1
+		elif student["Class"] == "Sophomore":
+			sophomore_size += 1
+		elif student["Class"] == "Freshman":
+			freshman_size += 1
+	class_size = [("Senior", senior_size), ("Junior", junior_size), ("Sophomore", sophomore_size), ("Freshman", freshman_size)]
+	return sorted(class_size, key = lambda x: x[1], reverse = True)
 
 def findMonth(a):
-# Find the most common birth month form this data
-# Input: list of dictionaries
-# Output: Return the month (1-12) that had the most births in the data
-
-	pass
+	birth_months = {}
+	birth_occurances = []
+	for x in a:
+		birthday = x["DOB"].split("/")
+		month = birthday[0]
+		birth_occurances.append(month)
+	for y in birth_occurances:
+		if y not in birth_months:
+			birth_months[y] = 0
+		birth_months[y] += 1
+	most_common = sorted(birth_months, key = lambda x: birth_months[x], reverse = True)
+	return int(most_common[0])
 
 def mySortPrint(a,col,fileName):
-#Similar to mySort, but instead of returning single
-#Student, the sorted data is saved to a csv file.
-# as fist,last,email
-#Input: list of dictionaries, col (key) to sort by and output file name
-#Output: No return value, but the file is written
-
-	pass
+	sorted_students2 = sorted(a, key = lambda x: x[col]) # Same sort as mySort
+	with open(fileName, "w") as e:
+		filewriter = csv.writer(e, delimiter = "," ) # seperating by commas 
+		for student in sorted_students2:
+			filewriter.writerow([student["First"], student["Last"], student["Email"]])
+	print("writing done")
+	e.close()
 
 def findAge(a):
-# def findAge(a):
-# Input: list of dictionaries
-# Output: Return the average age of the students and round that age to the nearest
-# integer.  You will need to work with the DOB and the current date to find the current
-# age in years.
-
-	pass
+	my_ages = []
+	for x in a:
+		dates = x["DOB"].split("/")
+		year = dates[2]
+		age = 2018 - int(year)
+		my_ages.append(age)
+	average_age = sum(my_ages)/len(my_ages)
+	return round(average_age)
 
 
 ################################################################
@@ -61,14 +94,14 @@ def findAge(a):
 
 ## We have provided simple test() function used in main() to print what each function returns vs. what it's supposed to return.
 def test(got, expected, pts):
-  score = 0;
-  if got == expected:
-    score = pts
-    print(" OK ", end=" ")
-  else:
-    print (" XX ", end=" ")
-  print("Got: ",got, "Expected: ",expected)
-  return score
+	score = 0
+	if got == expected:
+		score = pts
+		print(" OK ", end=" ")
+	else:
+		print (" XX ", end=" ")
+	print("Got: ",got, "Expected: ",expected)
+	return score
 
 
 # Provided main() calls the above functions with interesting inputs, using test() to check if each result is correct or not.
